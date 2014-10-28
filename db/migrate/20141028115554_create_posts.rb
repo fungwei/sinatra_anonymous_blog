fc87e0a763d57a20b2399abdb05a4188a8e3c952
Code Division <codedivision@11th.ventures>
2014-10-28 12:05:33 +0800

    added relationships and migration content

--- db/migrate/20141028115554_create_posts.rb
+++ db/migrate/20141028115554_create_posts.rb
@@ -1,4 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
+      create_table :posts do |t|
+        t.string :title, null: false
+        t.string :content
+
+      t.timestamps
+    end
   end
 end
